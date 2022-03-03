6b234f34ab102cbc9f3ff31902ddaead82c87a5a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-03 23:14:38 +0500

    second section in react

---
+++ test-app/src/components/secondSection.js
@@ -0,0 +1,37 @@
+function secondSection() {
+  return (
+    <tr>
+      <td>
+        <ul>
+          <li>Ara</li>
+          <li>Profile</li>
+          <li>Sheet</li>
+          <li>
+            170<sup>o</sup>
+          </li>
+        </ul>
+      </td>
+      <td>
+        <ul>
+          <li>Ara</li>
+          <li>Profile</li>
+          <li>Sheet</li>
+          <li>
+            170<sup>o</sup>
+          </li>
+        </ul>
+      </td>
+      <td>
+        <ul>
+          <li>Ara</li>
+          <li>Profile</li>
+          <li>Sheet</li>
+          <li>
+            170<sup>o</sup>
+          </li>
+        </ul>
+      </td>
+    </tr>
+  );
+}
+export default secondSection;
