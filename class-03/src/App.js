fac783ae71db8d84c872982e622ef12140001e07
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-10 11:21:54 +0500

    input data in react

--- class-03/src/App.js
+++ class-03/src/App.js
@@ -1,61 +1,13 @@
 import React, { useState } from "react";
-
+import FormHandling from "./Components/formHandling";
 // form handling
 // data maping
 
 function App() {
-  const [students, setStudents] = useState([
-    {
-      name: "awais",
-      batch: "Batch-3",
-      rollno: 5157,
-      class: "web and mobile"
-    },
-    {
-      name: "Hamza",
-      batch: "Batch-5",
-      rollno: 5157,
-      class: "web and mobile"
-    },
-    {
-      name: "waqas",
-      batch: "Batch-3",
-      rollno: 5147,
-      class: "web and mobile"
-    },
-  ]);
-  console.log("students", students);
+
   return (
     <div className="App">
-
-      {/* <p>count: {count}</p> */}
-      <h3>List of Students</h3>
-      <table>
-        <tr>
-          <th>No.</th>
-          <th>Student Name:</th>
-          <th>Student RollNo:</th>
-          <th> Student Class:</th>
-          <th>Student Batch:</th>
-        </tr>
-        {
-          students.map((students, index) => {
-            return (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{students.name}</td>
-                <td>{students.batch}</td>
-                <td> {students.rollno}</td>
-                <td>{students.class}</td>
-              </tr>
-            )
-
-
-          })
-        }
-
-      </table>
-      <hr />
+      <FormHandling />
     </div>
   );
 }
--- class-03/src/Components/formHandling.js
+++ class-03/src/Components/formHandling.js
@@ -4,15 +4,41 @@ import React, { useState } from "react";
 // data maping
 function FormHandling() {
     const [name, setName] = useState("");
-    const onChangeHandler = (e) => {
+    const [password, setpassword] = useState("");
+    const [studclass, setClass] = useState("");
+    const [batch, setBatch] = useState("");
+    const [roll, setRollNo] = useState("");
 
+
+
+    // const onChangeHandler = (e) => {
+    // }
+    const submitHandler = () => {
+        // stu onbject
+        let student = {
+            name: name,
+            password: password,
+            batch,
+            class: studclass,
+            roll,
+        }
+        console.log("students is", student);
 
     }
 
     return (
         <div className="App">
-            <input type="text" onChange={(e) => setName(e.target.value)} placeholder="enter your name" name="name"></input>
-            <p>Name is :{name}</p>
+            <input type="text" onChange={(e) => setName(e.target.value)} placeholder="enter your name" name="name"></input><br />
+            <input type="password" placeholder="password" onChange={(e) => setpassword(e.target.value)} /><br />
+            <input type="text" placeholder="enter your batch" onChange={(e) => setBatch(e.target.value)} /><br />
+            <input type="text" placeholder="enter your class" onChange={(e) => setClass(e.target.value)} /><br />
+            <input type="number" placeholder="Enter your Roll No." onChange={(e) => setRollNo(e.target.value)} /><br />
+            <button onClick={submitHandler}>Submit Data</button>
+            <h1>Name is :{name}</h1>
+            <h1>Password is :{password}</h1>
+            <h1>Your Batch is :{batch}</h1>
+            <h1>Your Class is :{studclass}</h1>
+            <h1>Your Roll No is :{roll}</h1>
 
         </div>
     );
