8c4b1d4508df12bf1029d59916cb9f3755d4f884
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-10 10:36:42 +0500

    add student data

--- class-03/src/App.js
+++ class-03/src/App.js
@@ -1,36 +1,33 @@
-import useState from "react";
-const [students, setStudents] = useState([
-  {
-    name: "awais",
-    batch: "Batch-03",
-    roll: 5157,
-    class: "web and mobile",
-  },
-  {
-    name: "Hamza",
-    batch: "Batch-04",
-    roll: 5156,
-    class: "web and mobile",
-  },
-  {
-    name: "Waqas",
-    batch: "Batch-06",
-    roll: 5153,
-    class: "web and mobile",
-  },
-  {
-    name: "Waqar",
-    batch: "Batch-02",
-    roll: 5158,
-    class: "web and mobile",
-  },
-]);
+import React, { useState } from "react";
+
 // form handling
 // data maping
 
 function App() {
+  const [students, setStudents] = useState([
+    {
+      name: "awais",
+      batch: "Batch-3",
+      rollno: 5157,
+      class: "web and mobile"
+    },
+    {
+      name: "Hamza",
+      batch: "Batch-5",
+      rollno: 5157,
+      class: "web and mobile"
+    },
+    {
+      name: "waqas",
+      batch: "Batch-3",
+      rollno: 5147,
+      class: "web and mobile"
+    },
+  ]);
+  console.log("students", students);
   return (
     <div className="App">
+
       {/* <p>count: {count}</p> */}
       <h3>List of Students</h3>
       <table>
