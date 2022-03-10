78842b3e19685f1d7ab590b60a2575371a26e8d7
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-10 09:55:55 +0500

    form handling and data maping

--- class-03/src/App.js
+++ class-03/src/App.js
@@ -1,38 +1,32 @@
 import useState from "react";
-import "./App.css";
-// let count = 0;
-// count = 5
-// const [count, setCount] = useState(0);
 const [students, setStudents] = useState([
   {
     name: "awais",
     batch: "Batch-03",
     roll: 5157,
-    class: "web and mobile"
-  }
-  ,
+    class: "web and mobile",
+  },
   {
     name: "Hamza",
     batch: "Batch-04",
     roll: 5156,
-    class: "web and mobile"
+    class: "web and mobile",
   },
   {
     name: "Waqas",
     batch: "Batch-06",
     roll: 5153,
-    class: "web and mobile"
-  }
-  , {
+    class: "web and mobile",
+  },
+  {
     name: "Waqar",
     batch: "Batch-02",
     roll: 5158,
-    class: "web and mobile"
-  }
-])
+    class: "web and mobile",
+  },
+]);
 // form handling
 // data maping
-
 
 function App() {
   return (
@@ -43,22 +37,21 @@ function App() {
         <tr>
           <th>Student Name:</th>
           <th>Student RollNo:</th>
-          <th>  Student Class:</th>
+          <th> Student Class:</th>
           <th>Student Batch:</th>
         </tr>
         <tr>
           <td>AWAIS</td>
           <td>5157</td>
-          <td>  Saylani</td>
+          <td> Saylani</td>
           <td>Batch-03</td>
         </tr>
         <tr>
           <td>AWAIS</td>
           <td>5157</td>
-          <td>  Saylani</td>
+          <td> Saylani</td>
           <td>Batch-03</td>
         </tr>
-
       </table>
       <hr />
     </div>
